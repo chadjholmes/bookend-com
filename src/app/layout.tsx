4d9f242f7d892b4d0034d6a6bfbe70e385a1c356
Chad Holmes <chad.holmes@pattern.com>
2024-11-02 11:17:00 -0600

    add icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,13 @@
 import './globals.css'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Bookends',
+  description: 'Your personal library, reading goals, and progress tracker in one place',
+  icons: {
+    icon: 'favicon.ico',
+  },
+}
 
 export default function RootLayout({
   children,
